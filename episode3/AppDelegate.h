e28582ed9c9a68ad5c84071ecbb9c3532eb9e1cf
Dimitar <admin@admins-Mac.local>
2014-11-08 03:10:15 -0800

    Initial Commit

---
+++ episode3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  episode3
+//
+//  Created by Dimitar on 11/8/14.
+//  Copyright (c) 2014 Dimitar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ episode3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  episode3
+//
+//  Created by Dimitar on 11/8/14.
+//  Copyright (c) 2014 Dimitar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
