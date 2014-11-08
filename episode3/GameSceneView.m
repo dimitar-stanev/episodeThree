8f3e5e2d0cbde9895d171394ccd93115d3d106fe
Ivan Raychev <ivanraychev@Ivans-Mac-mini.local>
2014-11-08 15:24:16 +0200

    added ui for game view and programmer

---
+++ episode3/GameSceneView.m
@@ -0,0 +1,21 @@
+//
+//  GameSceneView.m
+//  episode3
+//
+//  Created by Ivan Raychev on 11/8/14.
+//  Copyright (c) 2014 Dimitar. All rights reserved.
+//
+
+#import "GameSceneView.h"
+
+@implementation GameSceneView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ episode3/ProgrammerView.m
@@ -0,0 +1,21 @@
+//
+//  ProgrammerView.m
+//  episode3
+//
+//  Created by Ivan Raychev on 11/8/14.
+//  Copyright (c) 2014 Dimitar. All rights reserved.
+//
+
+#import "ProgrammerView.h"
+
+@implementation ProgrammerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
