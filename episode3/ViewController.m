30e0dccf589d84c65e02ee68b6a67a317a792b2a
Dimitar <admin@admins-Mac.local>
2014-11-08 07:10:59 -0800

    Added side menu. Added first button for side menu. Added sample tap handle.

--- episode3/ViewController.m
+++ episode3/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *testButton;
+@property (weak, nonatomic) IBOutlet UITableView *rightButtonsTable;
 
 @end
 
@@ -17,18 +18,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-- (IBAction)buttonClicked:(id)sender {
-    [UIView animateWithDuration:2 animations:^(void){
-        self.view.backgroundColor = [UIColor yellowColor];
-    }];
+    self.rightButtonsTable.delegate = self;
+    self.rightButtonsTable.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [btn setTitle:@"ASDF" forState:UIControlStateNormal];
+//    [cell addSubview:btn];
+    [cell setBackgroundView:btn];
+//    UIImageView *image = UIImage
+    UIImage *img = [UIImage imageNamed:@"womens_circle_2.jpg"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:img];
+    [cell setBackgroundView:imageView];
+//    [cell setBackgroundColor:[UIColor redColor]];
+//    [cell setMaskView:btn];
+//    [cell.textLabel setText:@"ASDF"];
+     return cell;
+ }
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"ROW SELECTED : %ld", (long)[indexPath row]);
+    [tableView reloadData];
 }
 
 @end
