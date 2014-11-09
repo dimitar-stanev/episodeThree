90501ca80a3321457ef0d5fdb02a3481e85d9b46
Ivan Raychev <ivanraychev@Ivans-Mac-mini.local>
2014-11-09 02:39:32 +0200

    finishing up programmer data class

--- episode3/GameSceneView.m
+++ episode3/GameSceneView.m
@@ -9,6 +9,7 @@
 #import "GameSceneView.h"
 #import "ProgrammerView.h"
 #import "Programmer.h"
+#import "ProgrammerStatusView.h"
 
 @interface GameSceneView()
 
@@ -19,8 +20,17 @@
 @property (weak, nonatomic) IBOutlet UIView *programmer5View;
 @property (weak, nonatomic) IBOutlet UIView *programmer6View;
 
+@property (weak, nonatomic) IBOutlet UIView *programmer1Status;
+@property (weak, nonatomic) IBOutlet UIView *programmer2Status;
+@property (weak, nonatomic) IBOutlet UIView *programmer3Status;
+@property (weak, nonatomic) IBOutlet UIView *programmer4Status;
+@property (weak, nonatomic) IBOutlet UIView *programmer5Status;
+@property (weak, nonatomic) IBOutlet UIView *programmer6Status;
+
 @property (strong, nonatomic) NSArray *programmerViews;
 @property (strong, nonatomic) NSArray *programmerButtons;
+@property (strong, nonatomic) NSArray *programmerStatuses;
+@property (strong, nonatomic) NSArray *programmerStatusViews;
 
 @end
 
@@ -31,21 +41,32 @@
     [super awakeFromNib];
     if ( self ) {
         self.programmerViews = @[_programmer1View, _programmer2View, _programmer3View, _programmer4View, _programmer5View, _programmer6View];
+        self.programmerStatuses = @[_programmer1Status, _programmer2Status, _programmer3Status, _programmer4Status, _programmer5Status, _programmer6Status];
         NSArray *directions = @[@(DirectionLeft), @(DirectionUp), @(DirectionRight), @(DirectionLeft), @(DirectionDown), @(DirectionRight)];
-                NSMutableArray *programmerButtons = [[NSMutableArray alloc] init];
         NSArray *images = @[@"character_fat_pink_one_140", @"character_fat_pink_one_140", @"character_fat_pink_one_140", @"character_fat_pink_one_140", @"character_fat_pink_one_140", @"character_fat_pink_one_140"];
+        NSArray *concentration = @[@6, @2, @2, @4, @4, @2];
+        NSArray *energy = @[@2, @6, @2, @4, @2, @4];
+        NSArray *health = @[@2, @2, @6, @2, @4, @4];
+        
+        NSMutableArray *programmerButtons = [[NSMutableArray alloc] init];
+        NSMutableArray *programmerStatusViews = [[NSMutableArray alloc] init];
         
         for ( int i = 0; i < 6; i++ ) {
-            Programmer *programmer = [[Programmer alloc] initWithImage:images[i] direction:[directions[i] intValue] concentration:4 energy:4 health:4];
+            Programmer *programmer = [[Programmer alloc] initWithImage:images[i] direction:[directions[i] intValue] concentration:[concentration[i] intValue] energy:[energy[i] intValue] health:[health[i] intValue]];
             
             ProgrammerView *programmerButton = [programmer programmerView];
             [programmerButton addTarget:self action:@selector(didPressProgrammerButton:) forControlEvents:UIControlEventTouchUpInside];
             [_programmerViews[i] addSubview:programmerButton];
             [programmerButtons addObject:programmerButton];
             
+            ProgrammerStatusView *statusView = [programmer programmerStatusView];
+            [_programmerStatuses[i] addSubview:statusView];
+            [programmerStatusViews addObject:statusView];
             
         }
         
+        _programmerButtons = [programmerButtons copy];
+        _programmerStatusViews = [programmerStatusViews copy];
         
     }
 }
